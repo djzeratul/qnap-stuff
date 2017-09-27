c0789f66f0b63b02f53d8cf41aea0edb27bd891a
Michael Klass <djzeratul@users.noreply.github.com>
2017-09-27 06:36:22 -0700

    logging for rar extract
    adding logging functionality to rar extractor, also adding logrotate on said logfile every 5MB

---
+++ shell/unrar_files.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#vars change for your setup
+script=/share/Container/rtorrent/config/scripts/unrar_logic.sh
+log_location=/share/Container/rtorrent/scripts/logs/extract.log
+#inst logfile
+touch $log_location
+#run logic script
+$script >> $log_location &
+pid=$!
+MaxFileSize=5242880
+while kill -0 $pid 2> /dev/null; do
+        #Get size in bytes 
+        file_size=`du -b $log_location | tr -s '\t' ' ' | cut -d' ' -f1`
+        if [ $file_size -gt $MaxFileSize ];then   
+            timestamp=`date +%F`
+            mv $log_location $log_location.$timestamp
+            touch $log_location
+        fi
+        sleep 1
+done
--- shell/unrar_logic.sh
+++ shell/unrar_logic.sh
@@ -3,38 +3,38 @@
 if [ "$(ls -d $1 | grep -F /data/download/torrent/)" ]; then
         #Create only 1 instance of this script to avoid rTorrent crashing
         if [ ! "$(ls /config/scripts | fgrep -i pidfile)" ]; then
-                yes no | nice -n 15 touch /config/scripts/pidfile
+                yes no | touch /config/scripts/pidfile
                 #Find and repeat for all folders and subfolders
                 for directory in $(find $1 -type d); do
                         #Check for .rar files and unpack them if found
                         if [ "$(ls $directory | fgrep -i .rar)" ]; then
                                 rarFile=`ls $directory | fgrep -i .rar`;
                                 searchPath="$directory/$rarFile"
-                                yes no | nice -n 15 unrar x -o+ $searchPath $directory
+                                yes no | unrar x -o+ $searchPath $directory
                         #Check for .001 files and unpack them if found
                         elif [ "$(ls $directory | fgrep -i .001)" ]; then
                                 rarFile=`ls $directory | fgrep -i .001`;
                                 searchPath="$directory/$rarFile"
-                                yes no | nice -n 15 unrar x -o+ $searchPath $directory
+                                yes no | unrar x -o+ $searchPath $directory
                         #Check for .zip files and unpack them if found
                         elif [ "$(ls $directory | fgrep -i .zip)" ]; then
                                 for zipFiles in `ls $directory | fgrep -i .zip`; do
                                         searchPath="$directory/$zipFiles"
-                                        yes no | nice -n 15 unzip -n $searchPath -d $directory
+                                        yes no | unzip -n $searchPath -d $directory
                                 done
                                 #When there is .zip files there are often .rar/.001 in them. Check and unpack if so
                                 if [ "$(ls $directory | fgrep -i .rar)" ]; then
                                 rarFile=`ls $directory | fgrep -i .rar`;
                                 searchPath="$directory/$rarFile"
-                                yes no | nice -n 15 unrar x -o+ $searchPath $directory
+                                yes no | unrar x -o+ $searchPath $directory
                                 #Check for .001 files and unpack them if found
                                 elif [ "$(ls $directory | fgrep -i .001)" ]; then
                                 rarFile=`ls $directory | fgrep -i .001`;
                                 searchPath="$directory/$rarFile"
-                                yes no | nice -n 15 unrar x -o+ $searchPath $directory
+                                yes no | unrar x -o+ $searchPath $directory
                                 fi
                         fi
                 done
-                yes no | nice -n 15 rm -f /config/scripts/pidfile
+                yes no | rm -f /config/scripts/pidfile
         fi
 fi
