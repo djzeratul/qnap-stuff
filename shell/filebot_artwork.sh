8ef5cfa5232a4dae604af652b28755cb7fe4c20d
Michael Klass <djzeratul@users.noreply.github.com>
2017-09-27 04:14:13 -0700

    major fixes
    fix infinte loop, add nice, add sleep to prevent constant filesize checking, only run loop while script is running so while loop will actually complete ;)

--- shell/filebot_artwork.sh
+++ shell/filebot_artwork.sh
@@ -2,10 +2,10 @@
 #log
 touch /share/Container/script/logs/artwork.log
 #Just run filebot artwork downloader on all files less than 1 day old
-filebot.sh -script fn:artwork.tmdb --def maxAgeDays=1 /share/HD/ >> /share/Container/script/logs/artwork.log &
+nice -n 15 filebot.sh -script fn:artwork.tmdb --def maxAgeDays=1 /share/HD/ >> /share/Container/script/logs/artwork.log &
+pid=$!
 MaxFileSize=5242880
-while true
-    do
+while kill -0 $pid 2> /dev/null; do
         #Get size in bytes** 
         file_size=`du -b /share/Container/script/logs/artwork.log | tr -s '\t' ' ' | cut -d' ' -f1`
         if [ $file_size -gt $MaxFileSize ];then   
@@ -13,4 +13,5 @@ while true
             mv /share/Container/script/logs/artwork.log /share/Container/script/logs/artwork.log.$timestamp
             touch /share/Container/script/logs/artwork.log
         fi
+        sleep 1
 done
